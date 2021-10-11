8cc99fc1928dd19cffa6f48678825f654633b8a2
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-10-12 00:31:45 +0300

    Initial Commit

---
+++ WritersReaders/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WritersReaders
+//
+//  Created by ПК on 12.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
