97a2dba4ee7ecf938bc91e5cbd1e9a2e71c074dd
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-10-12 11:52:09 +0300

    l

--- WritersReaders/main.cpp
+++ WritersReaders/main.cpp
@@ -1,74 +1,148 @@
-//
-//  main.cpp
-//  WritersReaders
-//
-//  Created by ПК on 12.10.2021.
-//
 #include <iostream>
-#include <iostream>
+#include <pthread.h>
+#include <fstream>
 #include <string>
-#include <vector>
-#include <mutex>
-#include <semaphore>
-#include <chrono>
-#include <thread>
+#include <unistd.h>
+#include <sys/time.h>
+#include <semaphore.h>
 using namespace std;
-int readcount=0, writecount=0;
-mutex mutex1, mutex2, w, r;
-
-class PROCESS {
-public:
-    void Reader() {
-        
-        while (true) {
-            this_thread::sleep_for(chrono::milliseconds(2000));
-        r.lock();
-        mutex1.lock();
-        readcount = readcount + 1;
-        if (readcount==1) w.lock();
-        mutex1.unlock();
-        r.unlock();
-
-        cout << "reader #" << readcount << "...читаем..." << endl;
-
-        mutex1.lock();
-        readcount = readcount - 1;
-        if (readcount == 0) w.unlock();
-        mutex1.unlock();
-        }
-    };
-    void Writter() {
-        
-        while (true) {
-            this_thread::sleep_for(chrono::milliseconds(8000));
-        mutex2.lock();
-        writecount = writecount + 1;
-        if (writecount==1) r.unlock();
-        mutex2.unlock();
-
-        w.lock();
-        cout << "...пишем..." << endl;
-        w.unlock();
-
-        mutex2.lock();
-        writecount = writecount - 1;
-        if (writecount == 0) r.unlock();
-        mutex2.unlock();
-        }
-    };
+// структура данных для вывода
+struct parameters_fuct
+{
+    string print_str;
 };
- 
+pthread_mutex_t semaf_mutex;     // семафор блокировки потоков записи
+sem_t count_write_sem;           // семафор ,счетчик записанных данных
+pthread_cond_t stop_read_thread; // семафор блокиратор потоков чтения
   
-int main() {
-    PROCESS process;
-    thread read([&]() {
-        process.Reader();
-    });
-    thread write([&]() {
-        process.Writter();
-    });
-
-    read.join();
-    write.join();
+// потоковая функция записи в фаил
+void *write_file(void *arg)
+{
+    struct parameters_fuct *p=(struct parameters_fuct*) arg;
+    ofstream line_write_file("buf_file.txt");   // получаем дескриптор фаил для записи
+    if(line_write_file)
+    {
+        for(;;){
+  
+        pthread_mutex_lock(&semaf_mutex); // блокируем один из потоков записи
+  
+        for(int i=0; i<100; i++)
+            {
+                // получаем текущее время
+                struct timeval tv;
+                struct timezone tz;
+                struct tm *tm;
+                gettimeofday(&tv, &tz);
+                tm=localtime(&tv.tv_sec);
+                // записуем его в фаил
+                line_write_file<<tm->tm_hour<<":"<<tm->tm_min<<":"<<tm->tm_sec<<":"<<tv.tv_usec<<"\t"<<p->print_str<<endl;
+                sem_post(&count_write_sem);// сообщаем что в файле появились данные
+            }
+        pthread_cond_signal(&stop_read_thread);// посылаем сигнал разблокировки потока чтения
+        pthread_mutex_unlock(&semaf_mutex); // разблокируем один из потоков записи
+        usleep(1500); // ожидаем , даем потоку считать данные
+        }
+    }
+    else
+    {
+        cout<<"Unable to open file for write"<<endl;
+    }
+  return NULL;
+}
+// потоковая функция чтения файла
+void *read_file(void *arg)
+{
+    ifstream line_read_file("/Users/pk/Desktop/OI/TASKS/WritersReaders/WritersReaders/Output.txt"); // получаем дескриптор файла для чтения
+  
+    if(line_read_file.is_open())
+    {
+        pthread_mutex_lock(&semaf_mutex);
+        pthread_cond_wait(&stop_read_thread,&semaf_mutex); // ожидаем если выполняется поток записи
+        for(;;)
+        {
+  
+     sem_wait(&count_write_sem); // ожидаем поступления данных в фаил
+  
+                string temp_string;
+                getline(line_read_file,temp_string); // получаем строку из файла
+                cout<<temp_string<<endl;
+                usleep(10000);
+  
+  
+        }
+         pthread_mutex_unlock(&semaf_mutex);
+    }
+  
+    else
+    {
+        cout<<"Unable to open file for read"<<endl;
+    }
+  return NULL;
+}
+int main()
+{
+    // установка счетчика записей в файле
+    sem_init(&count_write_sem,0,0);
+    // 4 потока
+    pthread_t thread_write_1;
+    pthread_t thread_write_2;
+    pthread_t thread_read_1;
+    pthread_t thread_read_2;
+  
+  
+    // структуры данных
+    struct parameters_fuct thread_write1_arg;
+    struct parameters_fuct thread_write2_arg;
+  
+    thread_write1_arg.print_str="Write thread first";
+    //создание потоков записи
+    int result=pthread_create(&thread_write_1,NULL,&write_file,&thread_write1_arg);
+    if(result!=0)
+    {
+        cout<<"Tread write 1 not create"<<endl;
+    }
+  
+    thread_write2_arg.print_str="Write second thread";
+    result=pthread_create(&thread_write_2,NULL,&write_file,&thread_write2_arg);
+    if(result!=0)
+    {
+        cout<<"Tread write 2 not create"<<endl;
+    }
+    //создание потоков чтения
+    result=pthread_create(&thread_read_1,NULL,&read_file,NULL);
+    if(result!=0)
+    {
+        cout<<"Tread read 1 create"<<endl;
+    }
+    result=pthread_create(&thread_read_2,NULL,&read_file,NULL);
+    if(result!=0)
+    {
+        cout<<"Tread read 2 create"<<endl;
+    }
+    // ожидание выполнения потоков
+    result = pthread_join(thread_read_1, NULL);
+    if (result != 0)
+     {
+        cout<<"Joining the first read thread"<<endl;
+        return 0;
+     }
+     result = pthread_join(thread_read_2, NULL);
+    if (result != 0)
+     {
+        cout<<"Joining the second read thread"<<endl;
+        return 0;
+     }
+     result=pthread_join(thread_write_1,NULL);
+     if(result!=0)
+     {
+         cout<<"Joining the first write thread"<<endl;
+         return 0;
+     }
+     result=pthread_join(thread_write_2,NULL);
+     if(result!=0)
+     {
+         cout<<"Joining the second write thread"<<endl;
+         return 0;
+     }
     return 0;
 }
